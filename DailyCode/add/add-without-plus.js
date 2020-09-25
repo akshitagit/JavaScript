2c0563a664695a0f4bc872562548aa866eeb4b85
Utkarsh Tiwari <utkarsh.tiwari@somaiya.edu>
2020-09-25 06:51:34 +0000

    completed add without + [issue #2]

--- DailyCode/add/add-without-plus.js
+++ DailyCode/add/add-without-plus.js
@@ -1,12 +1,10 @@
-function add(a, b){
-    while(b!=0){
-        carry = a & b
-
-        a = a ^ b
-
-        b = carry << 1
+function add(num1, num2){
+    while(num2!=0){
+        carry = num1 & num2 //carry
+        num1 = num1 ^ num2
+        num2 = carry << 1
     }
-    return a
+    return num1
 }
 
 console.log(add(5, 7))
