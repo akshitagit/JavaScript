85649d6c75040ea0df3c4aebcdee3d6a6e0357c3
Bilal Zahid <88459741+bilalzhd@users.noreply.github.com>
2022-08-06 13:26:18 +0500

    Create quicksort.js
    Just created  a quick sort algorithm add it

---
+++ Algorithms/quicksort.js
@@ -0,0 +1,16 @@
+function quicksort(array) {
+    if (array.length <= 1) {
+      return array;
+    }
+  
+    var pivot = array[0];
+    
+    var left = []; 
+    var right = [];
+  
+    for (var i = 1; i < array.length; i++) {
+      array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+    }
+  
+    return quicksort(left).concat(pivot, quicksort(right));
+  };
