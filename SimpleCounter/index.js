62eb4a1e4ba8deab1892ae00cc9e25934b530acb
Sandra Laguna <idsandrax@gmail.com>
2022-10-06 18:38:39 +0200

    add simple counter

---
+++ SimpleCounter/index.js
@@ -0,0 +1,13 @@
+var number = document.getElementById("value");
+
+function increment() {
+  number.value++;
+}
+
+function decrement() {
+  number.value--;
+}
+
+function reset() {
+  number.value = 0;
+}
