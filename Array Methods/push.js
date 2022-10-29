5a7e50a1d97f8f081b61f4effc84221bfe7cf605
japoncio3k <gustavo.kamihara@gmail.com>
2022-10-29 19:47:36 -0300

    Added Array/push.js

---
+++ Array Methods/push.js
@@ -0,0 +1,11 @@
+// Lets create a persons array with four elements,
+let people = ["Gustavo", "Lucio", "Julio", "Luan"];
+
+// removes the last element from the people array
+let length = people.push("Louise");
+
+console.log(`Output: ${people}`)         
+// Output: Gustavo,Lucio,Julio,Luan,Louise
+
+console.log(`Output: ${length}`)
+// Output: 5
