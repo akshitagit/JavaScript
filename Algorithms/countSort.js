8c2e649c6d6f0e31c9c0f9c1e312975f4f6be976
Bilal Zahid <88459741+bilalzhd@users.noreply.github.com>
2022-11-15 12:50:19 +0500

    Update countSort.js

--- Algorithms/countSort.js
+++ Algorithms/countSort.js
@@ -3,17 +3,14 @@ let countingSort = (arr) => {
         j = 0,
         len = arr.length,
         count = [];
-        let max = Math.min;
-    for(let i = 0; i<arr.length; i++){
-        if(arr[i] > max) max = arr[i];
-    }
-    for (i; i <= max; i++) {
+     let max = Math.max(...arr);
+     for (i; i <= max; i++) {
         count[i] = 0;
-    }
-    for (i = 0; i < len; i++) {
+     }
+     for (i = 0; i < len; i++) {
         count[arr[i]] += 1;
-    }
-    for (i = 0; i <= max; i++) {
+      }
+     for (i = 0; i <= max; i++) {
         while (count[i] > 0) {
             arr[j] = i;
             j++;
